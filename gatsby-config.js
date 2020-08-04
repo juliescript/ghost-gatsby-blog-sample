9ad8947815274cf86bac37a3ff64cc8dc7542a45
Julieta Campos <julieicelie@gmail.com>
2020-08-04 22:04:00 +0200

    Add Gatsby source

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,15 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-ghost`,
+      options: {
+          apiUrl: `https://api.juliescript.dev`,
+          contentApiKey: `01cdae7db7878bae65f9a0e0a8`,
+          version: `v3` // Ghost API version, optional, defaults to "v3".
+                        // Pass in "v2" if your Ghost install is not on 3.0 yet!!!
+      }
+   }
+  ],
 }
